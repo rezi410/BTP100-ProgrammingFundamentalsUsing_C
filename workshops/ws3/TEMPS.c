246060fce896746053ac42796f81d85a1298ff6f
Rezi <105070638+rezi410@users.noreply.github.com>
2022-05-28 23:20:10 -0400

    Add files via upload

---
+++ workshops/ws3/TEMPS.c
@@ -0,0 +1,45 @@
+//==============================================
+// Name:           Reziyemu Sulaiman
+// Student Number: 106-153-208
+// Email:          rsulaiman2@myseneca.ca
+// Section:        BTPNBB
+// Workshop:       3 (at-home)
+//==============================================
+
+#define _CRT_SECURE_NO_WARNINGS
+
+// Put your code below:
+#include <stdio.h>
+#define NUMS 4
+
+int main()
+{
+	int i, highValue, lowValue, highest, lowest, highDay, lowDay;
+	double avareage, maxTotal, minTotal;
+
+
+	printf("---=== IPC Temperature Analyzer ===---");
+
+	for (i = 1; i <= NUMS; i++)
+	{
+		int j = 0;
+		while (j == 0)
+		{
+			printf("\nEnter the high value for day %d: ", i);
+			scanf("%d", &highValue);
+			printf("\nEnter the low value for day %d: ", i);
+			scanf("%d", &lowValue);
+
+			if (highValue<41 && lowValue>-41 && highValue > lowValue)
+			{
+				j++;
+			}
+			else {
+				printf("Incorrect values, temperatures must be in the range -40 to 40, high must be greater than low.");
+			}
+
+		}
+	}
+
+	return 0;
+}
