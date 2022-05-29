d894952614380b976dc981f3dbec909cfe893c91
Rezi <105070638+rezi410@users.noreply.github.com>
2022-05-29 02:12:25 -0400

    Update contactHelpers.c

--- Assignments/A2/milestone/contactHelpers.c
+++ Assignments/A2/milestone/contactHelpers.c
@@ -1,13 +1,4 @@
-//==============================================
-// Name:           Reziyemu Sulaiman
-// Student Number: 106-153-208
-// Email:          rsulaiman2@myseneca.ca
-// Section:        NBB
-// Date:           11/25/2020
-//==============================================
-// Assignment:     2
-// Milestone:      4
-//==============================================
+
 
 #define _CRT_SECURE_NO_WARNINGS
 
